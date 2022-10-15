c469329dec6ea183bb376b2301661380596c3f09
Allan Debastiani <allandeba@yahoo.com.br>
2022-10-15 17:16:04 -0300

    Update uConfigurationView.pas

--- program/source/view/uConfigurationView.pas
+++ program/source/view/uConfigurationView.pas
@@ -18,8 +18,10 @@ type
   private
     procedure ValidateComponentRequire;
     procedure ValidateRetrievingTokenInformation;
+    procedure ValidateTokenInformation;
     procedure FillSessionManager;
     procedure ConfigureReloadImage;
+    procedure RetrieveTokenInformation;
   protected
     procedure PrepareComponents; override;
   end;
@@ -56,6 +58,20 @@ begin
 end;
 
 procedure TConfigurationView.ImageTokenInformationClick(Sender: TObject);
+begin
+  RetrieveTokenInformation;
+end;
+
+procedure TConfigurationView.PrepareComponents;
+begin
+  inherited;
+  AddImagesToADPasswordButtonedEdit(ADPasswordButtonedEditToken);
+  LabeledEditDomain.Text := TSessionManager.GetSessionInfo.Domain;
+  LabeledEditMainUsername.Text := TSessionManager.GetSessionInfo.MainUsername;
+  ConfigureReloadImage;
+end;
+
+procedure TConfigurationView.RetrieveTokenInformation;
 const
   PASSWORD_CHAR = #9;
 var
@@ -69,18 +85,11 @@ begin
   ADPasswordButtonedEditToken.Text := TTokenManager.GetToken(APassword);
 end;
 
-procedure TConfigurationView.PrepareComponents;
-begin
-  inherited;
-  AddImagesToADPasswordButtonedEdit(ADPasswordButtonedEditToken);
-  LabeledEditDomain.Text := TSessionManager.GetSessionInfo.Domain;
-  LabeledEditMainUsername.Text := TSessionManager.GetSessionInfo.MainUsername;
-  ConfigureReloadImage;
-end;
-
 procedure TConfigurationView.ValidateComponentRequire;
 begin
-  if ADPasswordButtonedEditToken.Text.IsEmpty or LabeledEditDomain.Text.IsEmpty or LabeledEditMainUsername.Text.IsEmpty then
+  ValidateTokenInformation;
+
+  if LabeledEditDomain.Text.IsEmpty or LabeledEditMainUsername.Text.IsEmpty then
     TMessageView.New(MSG_0010).ShowAndAbort;
 end;
 
@@ -91,6 +100,15 @@ begin
 
   if TMessageView.New(MSG_0014).Buttons([baYesNo]).Warning.ShowResult <> mrYes then
     Abort;
+end;
+
+procedure TConfigurationView.ValidateTokenInformation;
+begin
+  if ADPasswordButtonedEditToken.Text.IsEmpty then
+    if TMessageView.New(MSG_0010).Detail('Do you want to retrieve token from localfile?').Buttons([baYesNo]).Warning.ShowResult <> mrYes then
+      Abort;
+
+  RetrieveTokenInformation;
 end;
 
 end.
